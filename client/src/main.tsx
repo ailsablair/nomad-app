141dd97333988ebd27c9bc5508b8c65a5ab79824
Ailsa Blair <109297718+ailsablair@users.noreply.github.com>
2026-07-19 17:09:07 -0700

    Build: Fully configure Tailwind v4 and fix layout styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import { SeraProvider } from "@/components/ui/sera-provider";
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 );
-
-export default function App() {
-  return (
-    <SeraProvider defaultTheme="system" storageKey="nomad-theme">
-      <YourAppContent />
-    </SeraProvider>
-  );
-}
